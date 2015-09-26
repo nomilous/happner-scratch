94ccf0030066fa1a1dbb98ee4cec3751e7810aef
nomilous <nomilous@gmail.com>
2015-09-26 19:59:26 +0200

    commit

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # happner-scratch
+
+```
+sudo npm install happner --global
+
+happner --conf config/www.js
+happner --conf config/....js
+
+```
---
+++ config/www.js
@@ -0,0 +1,11 @@
+module.exports  .config = {
+  
+  version: '0.0.0',
+
+  // name: '__NAME__',
+  // port: 80,  // sudo...
+
+  components: {
+    'www': {}
+  }
+};
